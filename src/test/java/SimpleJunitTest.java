01a84e1041d1ba64b45441c9d1796a0518c401c5
Verisran <oskardobbz@gmail.com>
2024-11-06 19:44:57 +0000

    Added some sample unit tests

--- src/test/java/SimpleJunitTest.java
+++ src/test/java/SimpleJunitTest.java
@@ -1,12 +1,28 @@
 import org.junit.jupiter.api.*;
 
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleJunitTest
 {
+    //assert takes in 3 params: expected result, actual result, message when failed
+
     @Test
     void unitTest()
     {
-        System.out.println("Test Ran!");
+        assertEquals(5,4, "Inputs Not Equal");
+    }
+
+    @Test
+    void unitTest2()
+    {
+        int[] a = {1,4,5};
+        int[] b = {1,4,5};
+        assertArrayEquals(a,b, "Arrays are Not Equal");
+    }
+
+    @Test
+    void unitTest3()
+    {
+        assertTrue(5 == 5, "Inputs Not Equal");
     }
 }
