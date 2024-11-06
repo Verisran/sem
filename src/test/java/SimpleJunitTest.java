bb5249e6e508e9bb668f45a287824039c7f7583e
Verisran <oskardobbz@gmail.com>
2024-11-06 19:29:36 +0000

    Added JUnit testing to maven

---
+++ src/test/java/SimpleJunitTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.*;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleJunitTest
+{
+    @Test
+    void unitTest()
+    {
+        System.out.println("Test Ran!");
+    }
+}
