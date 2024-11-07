eb3cbc4c5e6c1ecef595b273ccff7d732dbf64ef
Reece <70919066+Locki-Locki@users.noreply.github.com>
2024-11-07 15:25:06 +0000

    Menu basic layed out, not functioning yet.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,11 +3,15 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class App
 {
+    //main of app
     public static void main(String[] args)
     {
+        // introduction to application running
+        System.out.println("---\tGROUP 33 SEM DATABASE INTERFACE\t---");
         try
         {
             // Load Database driver
@@ -24,7 +28,7 @@ public class App
         int retries = 100;
         for (int i = 0; i < retries; ++i)
         {
-            System.out.println("Connecting to database...");
+            System.out.println("\n---\tConnecting to database...\t---");
             try
             {
                 // Wait a bit for db to start
@@ -32,7 +36,7 @@ public class App
                 Thread.sleep(10000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "world");
-                System.out.println("Successfully connected");
+                System.out.println("---\tSuccessfully connected!\t---");
                 break;
             }
             catch (SQLException sqle)
@@ -46,8 +50,15 @@ public class App
             }
         }
 
-        if (con != null)
+        if (con != null) // connection is available on database
         {
+            System.out.println("notice: selection is wip and certain queries don't work.\n\n\n---\tWELCOME TO THE DATABASE INTERFACE!\t---");
+            boolean closed = false; // loop until exited out of menu
+
+            while (!closed) {
+                closed = menu(); // goes throw menu selection
+            }
+
             try
             {
                 // Close connection
@@ -58,5 +69,51 @@ public class App
                 System.out.println("Error closing connection to database");
             }
         }
+    }
+
+    public static boolean menu() {
+        boolean exit = false;
+        int selection = 0;
+
+        //selection message, will be shown each time there is a selection to be made
+        System.out.println(
+                "\n\t---\tplease make the following selections\t---"
+                + "\n 1 - Basic Population Queries\t 2 - All the x in y Queries"
+                + "\n3 - top N queries\t 4 - Complex population queries"
+                + "\n5 - reports\t 0 - quit"
+        );
+        Scanner sc = new Scanner(System.in);
+        selection = sc.nextInt();
+        switch (selection) {
+            case 1: // basic population queries
+
+                break;
+
+            case 2: // all x in y
+
+                break;
+
+            case 3: //top N queries
+
+                break;
+
+            case 4: //complex population queries
+
+                break;
+
+            case 5: //reports.
+
+                break;
+
+            case 0: // exit
+                exit = true;
+                break;
+
+            default: // error
+                System.out.println("Invalid selection! Try again.");
+                break;
+        }
+
+        return exit;
     }
 }
